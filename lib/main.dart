24271c5791df3168986208184c21df812b8eeb1a
f04k04369 <curb369@gmail.com>
2025-07-10 00:04:35 +0900

    アプリの日本語化対応その１

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('ja','JP'),
+      ],
 
+      home: HomeScreen(),
+    ),
+  );
+}
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-
-
-
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    Intl.defaultLocale = Localizations.localeOf(context).toString();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(),
+            Text(
+             DateFormat.yMEd().format(DateTime.now()), 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
