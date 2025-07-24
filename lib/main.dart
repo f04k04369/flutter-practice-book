7ce0866d39b889ce1d5fc947afb15050881a938b
f04k04369 <curb369@gmail.com>
2025-07-25 00:01:55 +0900

    navigate2.0wo

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,123 @@
 import 'package:flutter/material.dart';
-
-class MyTheme extends ThemeExtension<MyTheme> {
-  const MyTheme({
-    required this.themeColor,
-  });
-
-  final Color? themeColor;
-
-  @override
-  MyTheme copyWith({Color? themeColor}) {
-    return MyTheme(
-      themeColor: themeColor ?? this.themeColor);
-  }
-
-  @override 
-  MyTheme lerp(MyTheme? other, double t) {
-    if (other is! MyTheme) {
-      return this;
-    }
-    return MyTheme(
-      themeColor: Color.lerp(themeColor, other.themeColor, t),
-    );
-  }
-}
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp.router(
+      routerConfig: _router,
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  
-  @override
-  State<StatefulWidget> createState() => _MyAppState();
-}
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const FirstScreen(),
+    ),
+    GoRoute(
+      path: '/second',
+      builder: (context, state) => const SecondScreen(),
+    ),
+    GoRoute(
+      path: '/third',
+      builder: (context, state) => const ThirdScreen(),
+    ),
+  ],
+);
 
-class _MyAppState extends State<MyApp> {
-  bool _isDarkMode = false;
-
-  void _toggleDarkMode() {
-    setState(() {
-      _isDarkMode = !_isDarkMode;
-    });
-  }
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorSchemeSeed: Colors.green,
-        extensions: const [MyTheme(themeColor: Color(0xFF0000FF))],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FirstScreen'),
       ),
-      darkTheme: ThemeData(
-        colorSchemeSeed: Colors.green,
-        brightness: Brightness.dark,
-        extensions: const [MyTheme(themeColor: Color(0xFFFF0000))],
-      ),
-      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      home: Scaffold(
-        body: const Center(
-          child: ThemeWidget(),
-        ),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _toggleDarkMode();
-          },
-          child: const Icon(Icons.settings_brightness),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('FirstからSecontへ'),
+              onPressed: ( ) {
+                GoRouter.of(context).go('/second');
+              }, 
+            ),
+             ElevatedButton(
+              child: const Text('FirstからThirdへ'),
+              onPressed: ( ) {
+                GoRouter.of(context).go('/third');
+              }, 
+            ),
+         ],
         ),
       ),
     );
   }
-} 
+}
 
-class ThemeWidget extends StatelessWidget {
-  const ThemeWidget({super.key});
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final myTheme = Theme.of(context).extension<MyTheme>()!;
-    final color = myTheme.themeColor;
-    return Container(width: 100, height: 100, color: color);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SecondtScreen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('SecondからThirdへ'),
+              onPressed: ( ) {
+                GoRouter.of(context).go('/third');
+              }, 
+            ),
+             ElevatedButton(
+              child: const Text('戻る'),
+              onPressed: ( ) {
+                Navigator.of(context).pop();
+              }, 
+            ),
+         ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ThirdScreen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('ThirdからFirstへ'),
+              onPressed: ( ) {
+                GoRouter.of(context).go('/');
+              }, 
+            ),
+             ElevatedButton(
+              child: const Text('戻る'),
+              onPressed: ( ) {
+                Navigator.of(context).pop();
+              }, 
+            ),
+         ],
+        ),
+      ),
+    );
   }
 }
