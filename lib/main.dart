29836d0d004e37995b6459d98e84bf8166c1faa6
f04k04369 <curb369@gmail.com>
2025-07-24 21:20:30 +0900

    ダークモードとライトモードの切り替えをカスタムで動作させる

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      localizationsDelegates: [
-        GlobalWidgetsLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('ja','JP'),
-      ],
-      home: HomeScreen(),
-    ),
-  );
-}
+class MyTheme extends ThemeExtension<MyTheme> {
+  const MyTheme({
+    required this.themeColor,
+  });
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final Color? themeColor;
 
   @override
-  Widget build(BuildContext context) {
-    Intl.defaultLocale = Localizations.localeOf(context).toString();
-    return Scaffold(
-      body: Center(
-        child: Image.asset('assets/saku.png'),
-      ),
+  MyTheme copyWith({Color? themeColor}) {
+    return MyTheme(
+      themeColor: themeColor ?? this.themeColor);
+  }
+
+  @override 
+  MyTheme lerp(MyTheme? other, double t) {
+    if (other is! MyTheme) {
+      return this;
+    }
+    return MyTheme(
+      themeColor: Color.lerp(themeColor, other.themeColor, t),
     );
   }
 }
 
+void main() {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void _toggleDarkMode() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        extensions: const [MyTheme(themeColor: Color(0xFF0000FF))],
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        brightness: Brightness.dark,
+        extensions: const [MyTheme(themeColor: Color(0xFFFF0000))],
+      ),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        body: const Center(
+          child: ThemeWidget(),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _toggleDarkMode();
+          },
+          child: const Icon(Icons.settings_brightness),
+        ),
+      ),
+    );
+  }
+} 
+
+class ThemeWidget extends StatelessWidget {
+  const ThemeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final myTheme = Theme.of(context).extension<MyTheme>()!;
+    final color = myTheme.themeColor;
+    return Container(width: 100, height: 100, color: color);
+  }
+}
