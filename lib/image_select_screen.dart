f667dbc18e73d75eacf03d68eb6741ed345b0897
f04k04369 <curb369@gmail.com>
2025-08-08 16:50:21 +0900

    画像選択、デコード、リサイズ、エンコードまでの実装

--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,8 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:practice_book/l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
 
-class ImageSelectScreen extends StatelessWidget {
+class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
+
+  @override
+  State<ImageSelectScreen> createState => _ImageSelectScreensState();
+}
+
+class _ImageSelectScreensState extends State<ImageSelectScreen> {
+
+  final ImagePicker _picker = ImagePicker();
+
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    final XFile? imageFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap !=null);
+    if (imageBitmap == null) return;
+
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+    
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
